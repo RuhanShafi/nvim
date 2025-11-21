fc15b4531f34bd038b8a161e45b23df0134b8043
Ruhan Shafi <imruhans@gmail.com>
2025-11-21 23:45:31 +1100

    Configured Bufferline

--- lua/rsvim/plugins/ui.lua
+++ lua/rsvim/plugins/ui.lua
@@ -77,5 +77,115 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = 'v0.1.9',
     dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    event = "VeryLazy",
+    opts = {
+      options = {
+        mode = "buffers",
+        themable = true,
+      
+        -- Visual style
+        separator_style = "thin",
+        always_show_bufferline = true,
+        show_buffer_close_icons = true,
+        show_close_icon = true,
+        show_tab_indicators = true,
+
+        -- Close icons and mouse actions
+        close_command = "bdelete! %d",
+        right_mouse_command = "bdelete! %d",
+        left_mouse_command = "buffer %d",
+        middle_mouse_command = nil,
+
+        -- Diagnostics integration
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level)
+          local icon = level:match("error") and " " or " "
+          return " " .. icon .. count
+        end,
+
+        -- Integration with Neo-tree
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "File Explorer",
+            text_align = "center",
+            separator = true,
+          },
+        },
+         
+        -- Tab page support (for native tabs workflow)
+        show_buffer_default_icon = true,
+        color_icons = true,
+      },
+    },
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+      
+      local keymap = vim.keymap
+      
+      -- ========================================
+      -- BUFFER WORKFLOW KEYMAPS (<leader>b)
+      -- ========================================
+      
+      -- Navigate buffers
+      keymap.set("n", "<leader>bn", "<cmd>BufferLineCycleNext<CR>", 
+        { desc = "Next buffer" })
+      keymap.set("n", "<leader>bp", "<cmd>BufferLineCyclePrev<CR>", 
+        { desc = "Previous buffer" })
+    
+
+      -- Move buffers (reorder in bufferline)
+      keymap.set("n", "<leader>b>", "<cmd>BufferLineMoveNext<CR>", 
+        { desc = "Move buffer right" })
+      keymap.set("n", "<leader>b<", "<cmd>BufferLineMovePrev<CR>", 
+        { desc = "Move buffer left" })
+    
+      -- Close buffers
+      keymap.set("n", "<leader>bx", "<cmd>bdelete<CR>", 
+        { desc = "Close current buffer"})
+      keymap.set("n", "<leader>bX", "<cmd>BufferLineCloseOthers<CR>", 
+        { desc = "Close other buffers" })
+      keymap.set("n", "<leader>bc", "<cmd>BufferLinePickClose<CR>", 
+        { desc = "Pick buffer to close" })
+      keymap.set("n", "<leader>bh", "<cmd>BufferLineCloseLeft<CR>", 
+        { desc = "Close buffers to left" })
+      keymap.set("n", "<leader>bl", "<cmd>BufferLineCloseRight<CR>", 
+        { desc = "Close buffers to right" })
+
+            -- Pick/Jump to buffer
+      keymap.set("n", "<leader>bb", "<cmd>BufferLinePick<CR>", 
+        { desc = "Pick buffer" })
+      keymap.set("n", "<leader>bj", "<cmd>BufferLinePick<CR>", 
+        { desc = "Jump to buffer" })
+      
+      -- Pin/unpin buffer
+      keymap.set("n", "<leader>bP", "<cmd>BufferLineTogglePin<CR>", 
+        { desc = "Toggle pin buffer" })
+      
+      -- Sort buffers
+      keymap.set("n", "<leader>bse", "<cmd>BufferLineSortByExtension<CR>", 
+        { desc = "Sort by extension" })
+      keymap.set("n", "<leader>bsd", "<cmd>BufferLineSortByDirectory<CR>", 
+        { desc = "Sort by directory" })
+      keymap.set("n", "<leader>bsr", "<cmd>BufferLineSortByRelativeDirectory<CR>", 
+        { desc = "Sort by relative directory" })
+      keymap.set("n", "<leader>bst", "<cmd>BufferLineSortByTabs<CR>", 
+        { desc = "Sort by tabs" })
+
+      -- Additional tab-specific bufferline commands
+      keymap.set("n", "<leader>tt", "<cmd>BufferLineGroupToggle<CR>",
+        { desc = "Toggle buffer groups" })
+      keymap.set("n", "<leader>tg", "<cmd>BufferLineGroupClose<CR>",
+        { desc = "Close buffer group" })
+    end,
   }
 }
+
--- lua/rsvim/plugins/whichkey.lua
+++ lua/rsvim/plugins/whichkey.lua
@@ -25,22 +25,68 @@ return {
         g = true,
       },
     },
-
-    -- MAIN MAPPINGS
-    defaults = {
-      mode = "n",
-      ["<leader>"] = {
-
-        -- FILE EXPLORER GROUP
-        e = {
-          name = "File Tree",
-          e = { "<cmd>Neotree toggle<CR>", "Toggle file explorer" },
-          f = { "<cmd>Neotree reveal<CR>", "Toggle file explorer on current file" },
-          c = { "<cmd>Neotree close<CR><cmd>Neotree<CR>", "Collapse file explorer" },
-          r = { "<cmd>Neotree source=filesystem refresh<CR>", "Refresh file explorer" },
-        },
-      },
-    },
   },
-}
 
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+
+    -- Register all keymap groups
+    wk.add({
+      -- FILE EXPLORER GROUP
+      { "<leader>e", group = "File Tree" },
+      { "<leader>ee", desc = "Toggle file explorer" },
+      { "<leader>ef", desc = "Toggle file explorer on current file" },
+      { "<leader>ec", desc = "Collapse file explorer" },
+      { "<leader>er", desc = "Refresh file explorer" },
+
+      -- BUFFER WORKFLOW GROUP
+      { "<leader>b", group = "Buffers" },
+      { "<leader>bn", desc = "Next buffer" },
+      { "<leader>bp", desc = "Previous buffer" },
+      { "<leader>b>", desc = "Move buffer right" },
+      { "<leader>b<", desc = "Move buffer left" },
+      { "<leader>bx", desc = "Close current buffer" },
+      { "<leader>bX", desc = "Close other buffers" },
+      { "<leader>bc", desc = "Pick buffer to close" },
+      { "<leader>bh", desc = "Close buffers to left" },
+      { "<leader>bl", desc = "Close buffers to right" },
+      { "<leader>bb", desc = "Pick buffer" },
+      { "<leader>bj", desc = "Jump to buffer" },
+      { "<leader>bP", desc = "Toggle pin buffer" },
+      
+      -- Buffer sorting subgroup
+      { "<leader>bs", group = "Sort Buffers" },
+      { "<leader>bse", desc = "Sort by extension" },
+      { "<leader>bsd", desc = "Sort by directory" },
+      { "<leader>bsr", desc = "Sort by relative directory" },
+      { "<leader>bst", desc = "Sort by tabs" },
+      
+      -- TAB WORKFLOW GROUP (Native Neovim tabs)
+      { "<leader>t", group = "Tabs" },
+      { "<leader>to", desc = "Open new tab" },
+      { "<leader>tx", desc = "Close current tab" },
+      { "<leader>tn", desc = "Go to next tab" },
+      { "<leader>tp", desc = "Go to previous tab" },
+      { "<leader>tf", desc = "Open current buffer in new tab" },
+      { "<leader>tt", desc = "Toggle buffer groups" },
+      { "<leader>tg", desc = "Close buffer group" },
+
+      -- WINDOW/SPLIT GROUP
+      { "<leader>s", group = "Splits" },
+      { "<leader>sv", desc = "Split window vertically" },
+      { "<leader>sh", desc = "Split window horizontally" },
+      { "<leader>se", desc = "Make splits equal size" },
+      { "<leader>sx", desc = "Close current split" },
+
+      -- WORKSPACE/SESSION GROUP (from autosession)
+      { "<leader>w", group = "Workspace" },
+      { "<leader>wr", desc = "Restore session for cwd" },
+      { "<leader>ws", desc = "Save session for auto session root dir" },
+
+      -- OTHER GROUPS
+      { "<leader>n", group = "General" },
+      { "<leader>nh", desc = "Clear search highlights" },
+    })
+  end,
+}
